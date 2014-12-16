9e52fbd843c25edb15d0aef0feb5fe93df6dbdec
Andrew Hercules <andrewhercules@gmail.com>
2014-12-15 22:57:09 -0500

    First commit - created base app in Xcode and created README.md file

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Andrew Hercules on 2014-12-15.
+//  Copyright (c) 2014 Andrew Hercules. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
