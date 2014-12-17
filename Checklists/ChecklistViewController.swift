d4506609b2f07af1adcd4fd7480d405d5269c309
Andrew Hercules <andrewhercules@gmail.com>
2014-12-17 10:54:12 -0500

    Added two functions as part of UITableView's data source protocol

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,6 +10,14 @@ class ChecklistViewController: UITableViewController {
     super.didReceiveMemoryWarning()
   }
 
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem") as UITableViewCell
+    return cell
+  }
 
 }
 
