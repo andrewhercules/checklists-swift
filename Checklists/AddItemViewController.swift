f0eb23b9378e339bf0ab7e08c66320547d1da32e
Andrew Hercules <andrewhercules@gmail.com>
2014-12-17 15:48:09 -0500

    Disabled user from clicking done unless text field has new text

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+  
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  @IBOutlet weak var textField: UITextField!
+  
   @IBAction func cancel() {
     dismissViewControllerAnimated(true, completion: nil)
   }
   
   @IBAction func done() {
+    println("Contents of the text field: \(textField.text)")
     dismissViewControllerAnimated(true, completion: nil)
   }
-
+  
+  override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+    return nil
+  }
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    let oldText: NSString = textField.text
+    let newText: NSString = oldText.stringByReplacingCharactersInRange(
+      range, withString: string)
+    if newText.length > 0 {
+        doneBarButton.enabled = true
+      } else {
+        doneBarButton.enabled = false
+    }
+    return true
+  }
+  
 }
