4e9c33c83a7223bc686904e31a16d84156b47b55
Andrew Hercules <andrewhercules@gmail.com>
2014-12-17 15:01:33 -0500

    Created new table view and controller and created segues between the two views using bar button items

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Andrew Hercules on 2014-12-17.
+//  Copyright (c) 2014 Andrew Hercules. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+  @IBAction func cancel() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+  @IBAction func done() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+
+}
