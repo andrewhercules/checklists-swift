a721ea702a10a797c1b440848e33146116b54df2
Andrew Hercules <andrewhercules@gmail.com>
2014-12-19 12:30:10 -0500

    User can add items which display on Checklists view

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -90,22 +90,15 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     label.text = item.text
   }
   
-  @IBAction func addItem() {
-    let newRowIndex = items.count
-    let item = ChecklistItem()
-    item.text = "I am a new row"
-    item.checked = true
-    items.append(item)
-    
-    let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
-    let indexPaths = [indexPath]
-    tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
-  }
-  
   func addItemViewControllerDidCancel(controller: AddItemViewController) { dismissViewControllerAnimated(true, completion: nil)
   }
   
   func addItemViewController(controller: AddItemViewController, didFinishAddingItem item: ChecklistItem) {
+    let newRowIndex = items.count
+    items.append(item)
+    let indexPath = NSIndexPath(forRow: newRowIndex, inSection: 0)
+    let indexPaths = [indexPath]
+    tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Automatic)
     dismissViewControllerAnimated(true, completion: nil)
   }
 
