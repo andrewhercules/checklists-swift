7414e793a7d0404148e50e9df700c2cabc31a1ac
Andrew Hercules <andrewhercules@gmail.com>
2014-12-24 13:42:10 -0500

    Fixed minor issue with saving data to Checklists.plist file

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -6,20 +6,11 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-  
-  // MARK: - Table view data source
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return dataModel.lists.count
