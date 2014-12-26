be69f00a2199bc18ac13869a1343f1f61213f457
Andrew Hercules <andrewhercules@gmail.com>
2014-12-26 11:32:58 -0500

    Added icons for checklists and made noicon.png show by default

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -51,6 +51,9 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     } else {
       cell.detailTextLabel!.text = "\(count) Remaining"
     }
+    
+    cell.imageView!.image = UIImage(named: checklist.iconName)
+    
     return cell
   }
   
--- Checklists/Checklist.swift
+++ Checklists/Checklist.swift
@@ -3,21 +3,25 @@ import UIKit
 class Checklist: NSObject, NSCoding {
   var name = ""
   var items = [ChecklistItem]()
+  var iconName: String
   
   init(name: String) {
     self.name = name
+    iconName = "No Icon"
     super.init()
   }
   
   required init(coder aDecoder: NSCoder) {
     name = aDecoder.decodeObjectForKey("Name") as String
     items = aDecoder.decodeObjectForKey("Items") as [ChecklistItem]
+    iconName = aDecoder.decodeObjectForKey("IconName") as String
     super.init()
   }
   
   func encodeWithCoder(aCoder: NSCoder) {
     aCoder.encodeObject(name, forKey: "Name")
     aCoder.encodeObject(items, forKey: "Items")
+    aCoder.encodeObject(iconName, forKey: "IconName")
   }
   
   func countUncheckedItems() -> Int {
