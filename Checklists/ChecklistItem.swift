a262e0848d1b32282bfb0aa0ceb407c6807a0d1b
Andrew Hercules <andrewhercules@gmail.com>
2014-12-26 16:05:08 -0500

    Local notifications can be amended or deleted

--- Checklists/ChecklistItem.swift
+++ Checklists/ChecklistItem.swift
@@ -36,6 +36,11 @@ class ChecklistItem: NSObject, NSCoding {
   }
   
   func scheduleNotification() {
+    let existingNotification = notificationForThisItem()
+    if let notification = existingNotification {
+      println("Found an existing notification \(notification)")
+      UIApplication.sharedApplication().cancelLocalNotification(notification)
+    }
     if shouldRemind && dueDate.compare(NSDate()) != NSComparisonResult.OrderedAscending {
       let localNotification = UILocalNotification()
       localNotification.fireDate = dueDate
@@ -48,4 +53,24 @@ class ChecklistItem: NSObject, NSCoding {
     }
   }
   
+  func notificationForThisItem() -> UILocalNotification? {
+    let allNotifications = UIApplication.sharedApplication().scheduledLocalNotifications as [UILocalNotification]
+    for notification in allNotifications {
+      if let number = notification.userInfo?["ItemID"] as? NSNumber {
+        if number.integerValue == itemID {
+          return notification
+        }
+      }
+    }
+    return nil
+  }
+  
+  deinit {
+    let existingNotification = notificationForThisItem()
+    if let notification = existingNotification {
+      println("Removing existing notification \(notification)")
+      UIApplication.sharedApplication().cancelLocalNotification(notification)
+    }
+  }
+
 }
